5c0745f6be7bd79cd5dd1b42950db006dc268369
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-12-18 20:22:09 -0500

    updated models corresponding dao

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -14,9 +14,36 @@ public class InventoryDao {
 
     public void addVehicleToInventory(String vin, int dealershipId) {
         // TODO: Implement the logic to add a vehicle to the inventory
+        String query = "INSERT inventory VALUES (?, ?);";
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)){
+
+            statement.setInt(1, dealershipId);
+            statement.setString(2, vin);
+
+            int rows = statement.executeUpdate();
+            System.out.println("\nInventory updated.");
+            System.out.println("Rows updated: " + rows);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
         // TODO: Implement the logic to remove a vehicle from the inventory
+        String query = "DELETE FROM inventory WHERE VIN = ?;";
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)){
+
+            statement.setString(1, vin);
+
+            int rows = statement.executeUpdate();
+            System.out.println("\nVehicle removed from inventory.");
+            System.out.println("Rows updated: " + rows);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
